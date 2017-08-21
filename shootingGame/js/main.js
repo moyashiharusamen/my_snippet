9290db461bbc60a6c2bc2e3938c357b2c4d89ddb
masashi tominaga <m.tominaga@leihauoli.com>
2017-08-21 14:49:58 +0900

    Progress of shooting game file in Chapter 2 of Javascript

--- shootingGame/js/main.js
+++ shootingGame/js/main.js
@@ -2,12 +2,15 @@ var screenCanvas, info;
 var run = true;
 var fps =  1000 / 30;
 var mouse =  new Point();
+var ctx;
 
 
 window.onload = function(){
   screenCanvas = document.getElementById('screen');
   screenCanvas.width = 256;
   screenCanvas.height = 256;
+
+  ctx = screenCanvas.getContext('2d');
 
   screenCanvas.addEventListener('mousemove', mouseMove, true);
   window.addEventListener('keydown', keyDown, true);
@@ -16,6 +19,13 @@ window.onload = function(){
 
   (function(){
     info.innerHTML = mouse.x + ' : ' + mouse.y;
+
+    ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0, 0, 100, 0.75)';
+    ctx.arc(mouse.x, mouse.y, 10, 0, Math.PI * 2, false);
+    ctx.fill();
+
     if(run) setTimeout(arguments.callee, fps);
   })();
 };
